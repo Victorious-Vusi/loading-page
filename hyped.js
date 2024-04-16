5e0ee3b2b59d163d22d309e89a806f99c105ead8
Victorious-Vusi <95769654+Victorious-Vusi@users.noreply.github.com>
2024-04-16 02:57:24 +0200

    Add files via upload

---
+++ hyped.js
@@ -0,0 +1,9 @@
+const text1 = document.querySelector('#text1');
+const text2 = document.querySelector('#text2');
+const text3 = document.querySelector('#text3');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    text1.style.display = 'none';
+    text2.style.display = 'none';
+})
